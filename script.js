8ec01f615dd9d0c43a280e6e2565f1f6c0a853da
mathias-tad <84601874+mathias-tad@users.noreply.github.com>
2025-02-20 17:58:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const card = document.getElementsByClassName("card");
 const playCard = document.getElementById("play-card");
 
-const links = ['https://youtube.com/embed/7Nqt4vRQBFg?si=IznJxHf1PXN84lWm', 'https://youtube.com/embed/IIzQEAr3UGE?si=_q2zS3k0eN5S4RFo', 'https://youtube.com/embed/pd7wWgM_SEE?si=mLe1zYJgVjlDfVNf'];
+const links = ['https://www.youtube.com/embed/MUwzSwDFFq8?si=Gsn76kZx898tNsL1&rel=0',
+    'https://www.youtube.com/embed/o-F9qfivtOE?si=Q-eZwmpYZfpTdqtk&rel=0',
+    'https://youtube.com/embed/6kYRUsXtS4s?si=P4eXXU7jUy5HIWSe'];
 [...card].forEach((clk, indx) => {
     clk.addEventListener("click", () => {
         playCard.innerHTML += `
         <div class="top" id="close-modal">
             <i class="fa-solid fa-xmark"></i>
-        </div>
-        <iframe class="player" src=${links[indx]} frameborder="0"></iframe>`;
+        </div>`
+        playCard.innerHTML += `<iframe class="player" width="560" height="315" src="${links[indx]}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
         playCard.showModal()
         const closeModal = document.getElementById("close-modal");
         closeModal.addEventListener("click", () => {
